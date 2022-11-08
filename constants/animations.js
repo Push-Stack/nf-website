8fa2c8035ac1c0c2132eef32a776f3064d938611
Usama Bilal <usamabilal444@hotmail.com>
2022-11-08 15:30:42 +0500

    add framer-motion and page-transitions

---
+++ constants/animations.js
@@ -0,0 +1,15 @@
+export const pageTransitions = {
+  initialState: {
+    opacity: 0,
+  },
+  animateState: {
+    opacity: 1,
+  },
+  existState: {
+    opacity: 0,
+  },
+};
+
+export const transitionDuration = {
+  duration: 0.75,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { pageTransitions, transitionDuration } from "../constants/animations";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
-    <>
+    <AnimatePresence exitBeforeEnter>
       <Head>
         <title>Non Fungible Arcade</title>
       </Head>
-      <Component {...pageProps} />
-    </>
+      <motion.div
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        variants={pageTransitions}
+        transition={transitionDuration}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
