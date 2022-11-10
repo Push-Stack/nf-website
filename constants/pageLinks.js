12c0fd686724021d3a6e193498fea54de507fdce
Usama Bilal <usamabilal444@hotmail.com>
2022-11-10 19:01:38 +0500

    change domain url for pageLinks

--- constants/pageLinks.js
+++ constants/pageLinks.js
@@ -2,8 +2,8 @@ export const pageLinks = {
   header: "header",
   roadmap: "roadmap",
   faqs: "faqs",
-  play: "https://nonfungiblearcade.com/arcade-room/",
-  rebelRun: "https://nonfungiblearcade.com/rebel-run/",
+  play: "https://nfa.gg/arcade-room/",
+  rebelRun: "https://nfa.gg/rebel-run/",
   discord: "https://discord.gg/nfarcade",
   twitter: "https://twitter.com/NFArcade",
 };
