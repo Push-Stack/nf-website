d88d8c384e3920faecf7971845148bc6aaba9d2f
Usama Bilal <usamabilal444@hotmail.com>
2022-11-17 14:48:24 +0500

    remove rebel-run button

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -72,13 +72,6 @@ const Header = () => {
           >
             Start Playing
           </a>
-
-          <a
-            href={pageLinks.rebelRun}
-            className="uppercase bg-primary font-bold py-2 px-5 rounded-full hover:bg-primary/90 transition-all"
-          >
-            Rebel Run
-          </a>
         </div>
       </div>
     </header>
