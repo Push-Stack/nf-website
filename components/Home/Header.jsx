262bd5ff14ae8fec703a18e11263622486587a93
Usama Bilal <usamabilal444@hotmail.com>
2022-11-21 20:02:23 +0500

    add header

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 import { pageLinks } from "../../constants/pageLinks";
 import { Link } from "react-scroll";
-import HeaderImage from "../../public/static/header.png";
+import HeaderImage from "../../public/static/header-2.jpg";
 import Logo from "../../public/static/logo.png";
 
 const Header = () => {
   return (
     <header
-      className="pt-16 pb-40  w-full max-w-full   relative  overflow-hidden"
+      className="pt-16 pb-40 min-h-screen  w-full max-w-full   relative  overflow-hidden"
       id={pageLinks.header}
     >
       <Image
@@ -18,6 +18,7 @@ const Header = () => {
         className="z-10 object-cover"
         priority
       />
+      <div className="absolute z-20 inset-0 h-full w-full bg-black/70" />
       <div className="flex justify-center relative z-20 ">
         <ul className="text-white font-medium uppercase flex gap-6 sm:gap-32 text-md sm:text-lg">
           <li className="hover:text-gray-200">
