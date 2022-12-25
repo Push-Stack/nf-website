875a369df1af739fe0a427ac63f50ddd0daa59cd
Usama Bilal <usamabilal444@hotmail.com>
2022-12-25 19:47:15 +0500

    add security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src 'self';
+  style-src 'self';
+  font-src 'self' https://fonts.googleapis.com;
+  img-src 'self';  
+`;
+
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
 
@@ -13,6 +48,15 @@ const nextConfig = {
         source: "/rebel-run",
         destination: "/rebel-run/index.html",
         permanent: true,
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
       },
     ];
   },
