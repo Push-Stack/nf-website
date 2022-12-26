f745ce8eb3291b2af60bb8eec7012e32b56d21b3
Usama Bilal <usamabilal444@hotmail.com>
2022-12-27 00:54:01 +0500

    fix game security errors

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
   child-src 'self';
+  worker-src blob:;
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   font-src https://fonts.gstatic.com;
   img-src 'self';  
